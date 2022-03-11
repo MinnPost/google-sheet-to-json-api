3d8d087971ff25bc00e45c813b09e7be6659ddb9
Jonathan Stegall <jstegall@minnpost.com>
2022-03-11 13:11:57 -0600

    install jwt and make it available across the application

--- src/extensions.py
+++ src/extensions.py
@@ -1,8 +1,10 @@
+from src.jwt import jwt
 from src.cache import cache
 from src.logger import ParserLogger
 
 def register_extensions(app):
 
+    jwt.init_app(app)
     cache.init_app(app)
-
+    
     app.log = ParserLogger('parser_results').logger
